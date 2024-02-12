49942356c445961c604fc16f993dcac2895bf64b
Ashish Chakravarti <chakravartiashish2406@gmail.com>
2024-02-12 17:16:32 +0530

    product card added at shop page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,10 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+/* GET Shop page. */
+router.get('/shop', function(req, res, next) {
+  res.render('shop', { title: 'shop' });
+});
+
 module.exports = router;
