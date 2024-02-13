bed4e018da39fefc637ec14cae1a01c603683b81
Ashish Chakravarti <chakravartiashish2406@gmail.com>
2024-02-13 19:49:21 +0530

    added db file

---
+++ db/db.js
@@ -0,0 +1,21 @@
+const mysql = require("mysql");
+
+// MySQL Connection
+const connection = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "nafscare",
+});
+
+
+// Connect to MySQL
+connection.connect((err) => {
+  if (err) {
+    console.error("Error connecting to MySQL: " + err.stack);
+    return;
+  }
+
+  console.log("Connected to MySQL");
+
+});
