76dca9ec070c3fd0439aa5613ee3457a9fae258f
Ashish Chakravarti <chakravartiashish2406@gmail.com>
2024-02-13 14:34:48 +0530

    saving login sign up page

--- routes/index.js
+++ routes/index.js
@@ -1,27 +1,34 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
-
 
 /* GET Shop page. */
-router.get('/shop', function(req, res, next) {
-  res.render('shop', { title: 'shop' });
+router.get("/shop", function (req, res, next) {
+  res.render("shop", { title: "shop" });
 });
-
 
 /* GET Checkout page. */
-router.get('/checkout', function(req, res, next) {
-  res.render('checkout', { title: 'checkout' });
+router.get("/checkout", function (req, res, next) {
+  res.render("checkout", { title: "checkout" });
 });
 
-
 /* GET MyAccount page. */
-router.get('/account', function(req, res, next) {
-  res.render('myAccount', { title: 'MyAccount' });
+router.get("/account", function (req, res, next) {
+  res.render("myAccount", { title: "MyAccount" });
+});
+
+/* GET cart page. */
+router.get("/cart", function (req, res, next) {
+  res.render("cart", { title: "myCart" });
+});
+
+/* GET cart page. */
+router.get("/auth", function (req, res, next) {
+  res.render("authPage", { title: "Welcome to nafscare" });
 });
 
 module.exports = router;
